04c9f5b3d17158323563652758f790c2c738f640
sistemas_Fmo <sistemas@ferremayoristas.com.mx>
2025-12-01 19:19:17 -0600

    fixed

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(process.env.PORT ?? 4201);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
