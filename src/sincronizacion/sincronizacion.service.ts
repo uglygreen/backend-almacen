1e52b7781587e87de3c84aea9872aa84dc3620f8
sistemas_Fmo <sistemas@ferremayoristas.com.mx>
2025-12-05 17:01:26 -0600

    cronTime 10s

--- src/sincronizacion/sincronizacion.service.ts
+++ src/sincronizacion/sincronizacion.service.ts
@@ -32,7 +32,7 @@ export class SincronizacionService {
   ) {}
 
   // Se ejecuta cada 60 segundos
-  @Cron(CronExpression.EVERY_30_SECONDS)
+  @Cron(CronExpression.EVERY_10_SECONDS)
   async sincronizarPedidos() {
 
      if (this.isSyncing) {
