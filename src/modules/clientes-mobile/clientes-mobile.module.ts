d314be1084b54e1c3652fbb55e4c5d0bb4fede16
sistemas_Fmo <sistemas@ferremayoristas.com.mx>
2026-06-16 18:42:34 -0600

    fixed

--- src/modules/clientes-mobile/clientes-mobile.module.ts
+++ src/modules/clientes-mobile/clientes-mobile.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Cliente, ClienteMobileOtp, ClienteMobileSession, CorreoLegacy, DocLegacy } from '../../entities';
+import { Cliente, ClienteMobileOtp, ClienteMobileSession, CorreoLegacy, DocLegacy, Personal } from '../../entities';
 import { ClientesMobileAuthGuard } from './clientes-mobile-auth.guard';
 import { ClientesMobileController } from './clientes-mobile.controller';
 import { ClientesMobileMailService } from './clientes-mobile-mail.service';
@@ -10,7 +10,7 @@ import { ClientesMobileService } from './clientes-mobile.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Cliente, CorreoLegacy, DocLegacy], 'legacy_db'),
+    TypeOrmModule.forFeature([Cliente, Personal, CorreoLegacy, DocLegacy], 'legacy_db'),
     TypeOrmModule.forFeature([ClienteMobileOtp, ClienteMobileSession]),
     JwtModule.register({}),
   ],
